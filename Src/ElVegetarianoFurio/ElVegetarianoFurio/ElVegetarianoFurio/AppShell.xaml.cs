49a870483c6cdf8643c1b464949889bc02b3b8e1
André Krämer <andre.kraemer@qualitybytes.de>
2020-05-03 17:56:41 +0200

    Finished 04-03
    Neuen Menüeintrag hinzugefügt, der via Xamarin.Essentials die Telefonie-App startet um einen Anruf zu tätigen

--- Src/ElVegetarianoFurio/ElVegetarianoFurio/ElVegetarianoFurio/AppShell.xaml.cs
+++ Src/ElVegetarianoFurio/ElVegetarianoFurio/ElVegetarianoFurio/AppShell.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,18 @@ namespace ElVegetarianoFurio
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AppShell : Shell
     {
+
+        public ICommand CallNowCommand => new Command(CallNow);
+
         public AppShell()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void CallNow()
+        {
+            PhoneDialer.Open("0123456789");
         }
     }
 }
