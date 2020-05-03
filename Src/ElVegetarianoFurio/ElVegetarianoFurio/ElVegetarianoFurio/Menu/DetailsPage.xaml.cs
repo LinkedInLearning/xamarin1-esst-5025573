de179731475d4f3574a55117fb352d7d6aef0594
André Krämer <andre.kraemer@qualitybytes.de>
2020-05-03 20:59:55 +0200

    Finished 07-05
    Detailseite für Speisen inklusive ViewModel erstellt

--- Src/ElVegetarianoFurio/ElVegetarianoFurio/ElVegetarianoFurio/Menu/DetailsPage.xaml.cs
+++ Src/ElVegetarianoFurio/ElVegetarianoFurio/ElVegetarianoFurio/Menu/DetailsPage.xaml.cs
@@ -10,11 +10,23 @@ using Xamarin.Forms.Xaml;
 namespace ElVegetarianoFurio.Menu
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [QueryProperty("DishId", "id")]
     public partial class DetailsPage : ContentPage
     {
         public DetailsPage()
         {
             InitializeComponent();
+        }
+
+        public string DishId
+        {
+            set
+            {
+                if (int.TryParse(value, out var dishId))
+                {
+                    BindingContext = new DetailsPageViewModel(dishId);
+                }
+            }
         }
     }
 }
---
+++ Src/ElVegetarianoFurio/ElVegetarianoFurio/ElVegetarianoFurio/Menu/DetailsPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ElVegetarianoFurio.Data;
+
+namespace ElVegetarianoFurio.Menu
+{
+    public class DetailsPageViewModel : INotifyPropertyChanged
+    {
+
+        private Dish _dish;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DetailsPageViewModel(int id)
+        {
+            var dataService = new DataService();
+            Dish = dataService.GetDishes().First(d => d.Id == id);
+        }
+
+        public Dish Dish
+        {
+            get
+            {
+                return _dish;
+            }
+            set
+            {
+                _dish = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
