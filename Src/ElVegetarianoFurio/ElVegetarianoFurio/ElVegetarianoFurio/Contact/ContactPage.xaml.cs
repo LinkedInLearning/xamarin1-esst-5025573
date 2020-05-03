a5b576c3aa208f633b8599e6d76cb021b7c787b4
André Krämer <andre.kraemer@qualitybytes.de>
2020-05-03 21:24:38 +0200

    Finished 07-07
    Navigation via Xamarin.Essentials.Maps eingebaut

---
+++ Src/ElVegetarianoFurio/ElVegetarianoFurio/ElVegetarianoFurio/Contact/ContactPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace ElVegetarianoFurio.Contact
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactPage : ContentPage
+    {
+        public ContactPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var placemark = new Placemark
+            {
+                CountryName = "Germany",
+                PostalCode = "53489",
+                Locality = "Sinzig",
+                Thoroughfare = "Barbarossastraße 2"
+            };
+
+            var options = new MapLaunchOptions
+            {
+                Name = "El Vegetariano Furio",
+                NavigationMode = NavigationMode.Driving
+            };
+            Map.OpenAsync(placemark, options);
+        }
+    }
+}
